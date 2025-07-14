d22636f679fbf7ab1d7bbacd5010c3cb595c4dd5
niloofar <sn.mousavi77@gmail.com>
2025-07-14 14:48:11 +0330

    footer changes

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,39 +1,75 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Chip, Typography } from "@mui/material";
 import HeroSection from "../global/heroSection";
 import ProductSection from "../productsSection";
-import CustomTabs from "../global/customTabs";
 import Features from "../features";
 import BestBrands from "../bestBrands";
-import logo from '@images/react.svg';
+
+import { FiShoppingBag } from "react-icons/fi";
+import { PiSneakerMoveBold } from "react-icons/pi";
+import { LiaTshirtSolid } from "react-icons/lia";
+import { TbSunglasses } from "react-icons/tb";
+import { GiDiamondRing } from "react-icons/gi";
 
 export default function Landing() {
+  const categories = [
+    { icon: <FiShoppingBag />, label: "Bags" },
+    { icon: <PiSneakerMoveBold />, label: "Shoes" },
+    { icon: <LiaTshirtSolid />, label: "Clothes" },
+    { icon: <TbSunglasses />, label: "Accessories" },
+    { icon: <GiDiamondRing />, label: "Jewelry" },
+   
+  ];
   return (
     <>
-      <Box sx={{ display: "flex", flexDirection: "column",gap:2 }}>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
         <HeroSection></HeroSection>
         <Features></Features>
         <BestBrands></BestBrands>
-        <Typography variant="h6" sx={{m:2}}>Great Deals on What You Love</Typography>
-        <Typography variant="h6" sx={{color:"gray",m:2}}>
+        <Typography variant="h6" sx={{ m: 2 }}>
+          Great Deals on What You Love
+        </Typography>
+        <Typography variant="h6" sx={{ color: "gray", m: 2 }}>
           Items similar to what you've browsed are on sale
         </Typography>
         {/* <CustomTabs></CustomTabs> */}
-        <Typography variant="h4" sx={{m:2}}>Newest Products</Typography>
+        <Typography variant="h4" sx={{ m: 2 }}>
+          Newest Products
+        </Typography>
         <ProductSection></ProductSection>
-          <Typography variant="h4" sx={{m:2}}>Categories</Typography>
-          <Box sx={{display:"flex",direction:"column"}}>
-            <img></img>
-            <Typography variant="h6" sx={{color:"gray",m:2}}>
-              Bags
-            </Typography>
-             
-
-
-          </Box>
-    
-
-       
-
+        <Typography variant="h4" sx={{ m: 2 }}>
+          Categories
+        </Typography>
+        <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: 2,
+        m: 2,
+      }}
+    >
+      {categories.map((item, index) => (
+        <Chip
+          key={index}
+          icon={item.icon}
+          label={item.label}
+          variant="outlined"
+          sx={{
+            borderRadius: "12px",
+            px: 2,
+            py: 1,
+            fontSize: "14px",
+            color: "black",
+            borderColor: "black",
+            "&:hover": {
+              backgroundColor: "primary.light",
+              color: "white",
+              borderColor: "primary.light",
+            },
+          }}
+        />
+      ))}
+        <ProductSection></ProductSection>
+    </Box>
 
       </Box>
     </>
--- src/components/global/footer/index.jsx
+++ src/components/global/footer/index.jsx
@@ -1,18 +1,41 @@
-import { Box, Typography, Link } from '@mui/material';
+import { Box, Typography, Link } from "@mui/material";
 
 const Footer = () => {
   return (
     <Box
       sx={{
-        backgroundColor: "black", // MUI primary color
-        color: 'white',
-        padding: '16px',
-        textAlign: 'center',
-        position: 'relative', // Position relative for proper stacking
+        backgroundColor: "black",
+        color: "white",
+        padding: "16px",
+        textAlign: "center",
+        position: "relative",
         bottom: 0,
-        width: '100%',
+        width: "100%",
       }}
     >
+<Box sx={{ display: "flex", flexDirection: "row", gap: 6 }}>
+  {/* Column 1 */}
+  <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+    <Typography variant="body1">About us</Typography>
+    <Typography variant="body1">Blog</Typography>
+    <Typography variant="body1">Contact us</Typography>
+  </Box>
+
+  {/* Column 2 */}
+  <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+    <Typography variant="body1">Woman</Typography>
+    <Typography variant="body1">Kids</Typography>
+    <Typography variant="body1"> Men</Typography>
+  </Box>
+
+  {/* Column 3 */}
+  <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+    <Typography variant="body1">Bags</Typography>
+    <Typography variant="body1">Shoes</Typography>
+    <Typography variant="body1"> Clothes</Typography>
+  </Box>
+</Box>
+
       <Typography variant="body2">
         © {new Date().getFullYear()} Your Company Name. All rights reserved.
       </Typography>
